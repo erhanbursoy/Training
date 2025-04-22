175fdc4da54910d9829339adc6289fd4e7dc9db1
Erhan Bursoy <erhanbursoy@gmail.com>
2025-04-22 07:18:07 +0000

    Initial setup for Node.js & React training

---
+++ react/01-hooks-basics/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Counter from './Counter';
+
+export default function App() {
+  return (
+    <div>
+      <h1>React Hooks Basics</h1>
+      <Counter />
+    </div>
+  );
+}
